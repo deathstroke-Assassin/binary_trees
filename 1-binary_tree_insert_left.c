5efeff876edb390b9befb8de1190e18d3639e116
deathstroke-Assassin <moh2004amed@gmail.com>
2024-02-29 15:21:36 -0800

    gv

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,28 +1,32 @@
 #include "binary_trees.h"
 
 /**
- *  binary_tree_insert_left - creates a left binary tree node
+ * binary_tree_insert_left - function that creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
- * Return: Pointer to the newly created node
- * NULL on failure
+ * Return: pointer to the new node, or NULL on failure
  */
-
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_t;
+	binary_tree_t *nodo = NULL;
 
-	new_t = malloc(sizeof(binary_tree_t));
-
-	if (new_t == NULL || parent == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	new_t->parent = parent;
-	new_t->right = NULL;
-	new_t->left = parent->left;
-	new_t->n = value;
-	if (parent->left != NULL)
-		new_t->left->parent = new_t;
-	parent->left = new_t;
-	return (new_t);
+	nodo = binary_tree_node(parent, value);
+	if (nodo == NULL)
+		return (NULL);
+
+	if (parent->left == NULL)
+	{
+		parent->left = nodo;
+	}
+	else
+	{
+		nodo->left = parent->left;
+		parent->left = nodo;
+		nodo->left->parent = nodo;
+	}
+
+	return (nodo);
 }
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,29 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - creates a binary tree node
+ * binary_tree_insert_right - function that creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
- * Return: Pointer to the newly created node
- * NULL on failure
+ * Return: pointer to the new node, or NULL on failure
  */
-
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_t;
+	binary_tree_t *nodo = NULL;
 
-	new_t = malloc(sizeof(binary_tree_t));
+	if (parent == NULL)
+		return (NULL);
 
-	if (!new_t)
+	nodo = binary_tree_node(parent, value);
+	if (nodo == NULL)
 		return (NULL);
-	if (!parent)
-		return (NULL);
-	new_t->parent = parent;
-	new_t->right = parent->right;
-	new_t->left = NULL;
-	new_t->n = value;
-	parent->right = new_t;
-	if (parent->right != NULL)
-		parent->right->parent = new_t;
-	return (new_t);
+
+	if (parent->right == NULL)
+	{
+		parent->right = nodo;
+	}
+	else
+	{
+		nodo->right = parent->right;
+		parent->right = nodo;
+		nodo->right->parent = nodo;
+	}
+
+	return (nodo);
 }
