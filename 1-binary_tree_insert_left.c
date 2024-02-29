a8e0887bed5f5155e940278a19c8d1d8316df46c
deathstroke-Assassin <moh2004amed@gmail.com>
2024-02-29 13:56:47 -0800

    ddd

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,18 +10,18 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_t;
+	binary_tree_t *new_t;
 
-        new_t = malloc(sizeof(binary_tree_t));
+	new_t = malloc(sizeof(binary_tree_t));
 
-        if ((!new_t) || (!parent))
-                return (NULL);
-        new_t->parent = parent;
-        new_t->right = NULL;
-        new_t->left = parent->left;
-        new_t->n = value;
+	if ((!new_t) || (!parent))
+	return (NULL);
+	new_t->parent = parent;
+	new_t->right = NULL;
+	new_t->left = parent->left;
+	new_t->n = value;
 	if (parent->left != NULL)
 		parent->left->parent = new_t;
 	parent->left = new_t;
-        return (new_t);
+	return (new_t);
 }
